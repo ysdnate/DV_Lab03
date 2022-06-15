0a1c9d3cb6dd30f16477cc2a77574f68aa992e51
Nhattoan Do <89866103+ysdnate@users.noreply.github.com>
2022-06-15 22:58:01 +0700

    Add files via upload

---
+++ Lab_3_carmpg.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 21:53:34 2015
+
+@author: nymph
+"""
+
+
+#################################### Read the data ############################
+import pandas as pd
+from pandas import DataFrame, Series
+import seaborn as sns
+import numpy as np
+
+''' read_csv()
+The read_csv() function in pandas package parse an csv data as a DataFrame data structure. What's the endpoint of the data?
+The data structure is able to deal with complex table data whose attributes are of all data types. 
+Row names, column names in the dataframe can be used to index data.
+'''
+
+data = pd.read_csv("http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data-original", delim_whitespace = True, \
+ header=None, names = ['mpg', 'cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'model', 'origin', 'car_name'])
+
+data['mpg']
+data.mpg
+data.iloc[0,:]
+
+print(data.shape)
+
+################################## Enter your code below ######################
---
+++ Lab_3_electricpower.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 02:33:19 2015
+
+@author: nymph
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+############################## Your code for loading and preprocess the data ##
+
+
+############################ Complete the following 4 functions ###############
+def plot1():
+    pass
+
+def plot2():
+    pass
+
+def plot3():
+    pass
+
+def plot4():
+    pass
+
